0b5d5baf82e69950c88072b4345a3922c7804e6f
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-26 19:43:57 -0400

    Curried Add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,12 @@
-function curriedAdd(total) {
-
+function curriedAdd(total = 0) {
+    if (arguments.length === 0) return total;
+    else if (typeof arguments[0] === 'number') {
+        return function recursiveAdd(num) {
+            if (!num) return total;
+            total += num;
+            return recursiveAdd;
+        }
+    }
 }
 
 module.exports = { curriedAdd };
