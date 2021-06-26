b8f610ed47639d812c0f3e6562efde67427aa420
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-26 18:02:45 -0400

    Bank Account

--- create-account.js
+++ create-account.js
@@ -1,5 +1,39 @@
-function createAccount(pin, amount) {
+function createAccount(p, amount) {
+    let pin = p,
+        balance = amount || 0,
+        wrongPinMsg = "Invalid PIN.";
 
+    return {
+        checkBalance: function(p) {
+            if (p === pin) return `$${balance}`;
+            else return wrongPinMsg;
+        },
+        deposit: function(p, amt) {
+            if (p === pin) {
+                balance += amt;
+                return `Succesfully deposited $${amt}. Current balance: $${balance}.`
+            } else {
+                return wrongPinMsg;
+            }
+        },
+        withdraw: function(p, amt) {
+            if (p === pin) {
+                if (amt > balance) return "Withdrawal amount exceeds account balance. Transaction cancelled."
+                balance -= amt;
+                return `Succesfully withdrew $${amt}. Current balance: $${balance}.`
+            } else {
+                return wrongPinMsg;
+            }
+        },
+        changePin: function(p, newP) {
+            if (p === pin) {
+                pin = newP;
+                return "PIN successfully changed!";
+            } else {
+                return wrongPinMsg;
+            }
+        }
+    }
 }
 
 module.exports = { createAccount };
